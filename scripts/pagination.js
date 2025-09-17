dbaee951351596fe94e173f2fd4e33224f404712
lyumis <anyutik.morozik@gmail.com>
2025-09-18 01:33:39 +0300

    first commit

---
+++ scripts/pagination.js
@@ -0,0 +1,432 @@
+
+const pets = [
+  {
+    image:
+      "./src/img/cats/cat-1-1-archick.jpg",
+    name: "Арчик",
+    gender: "male",
+    feature: "Метис, 2 месяца"
+  },
+  {
+    // link:
+    //   "pet-page.html",
+    image:
+      "./src/img/cats/cat-1-1-klyaksa.jpg",
+    name: "Клякса",
+    gender: "",
+    feature: "Метис, 4 года 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-funtik.jpg",
+    name: "Фунтик",
+    gender: "male",
+    feature: "Метис, 3 года 4 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-tasya.jpg",
+    name: "Тася",
+    gender: "",
+    feature: "Метис, 7 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-malysh.jpg",
+    name: "Малыш",
+    gender: "male",
+    feature: "Метис, 4 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-joja.jpg",
+    name: "Жожа",
+    gender: "male",
+    feature: "Метис, 5 лет 7 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-lisenok.jpg",
+    name: "Лисенок",
+    gender: "male",
+    feature: "Метис, 5 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-dymka.jpg",
+    name: "Дымка",
+    gender: "",
+    feature: "Метис, 7 лет 1 месяц"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-gloria.jpg",
+    name: "Глория",
+    gender: "",
+    feature: "Метис, 8 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-carl.jpg",
+    name: "Карл",
+    gender: "male",
+    feature: "Метис, 4 года 4 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-chapik.jpg",
+    name: "Чапик",
+    gender: "male",
+    feature: "Метис, 7 лет 1 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-oscar.jpg",
+    name: "Оскар",
+    gender: "male",
+    feature: "Метис, 6 лет 6 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-tykovka.jpg",
+    name: "Тыковка",
+    gender: "",
+    feature: "Метис, 1 месяц"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-linda.jpeg",
+    name: "Линда",
+    gender: "",
+    feature: "Метис, 3 лет 10 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-belyash.jpeg",
+    name: "Беляш",
+    gender: "male",
+    feature: "Метис, 7 лет 3 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-gercoginya.jpg",
+    name: "Герцогиня",
+    gender: "",
+    feature: "Метис, 9 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-shapka.jpg",
+    name: "Шапка",
+    gender: "male",
+    feature: "Метис, 4 года 11 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-sonya.jpg",
+    name: "Соня",
+    gender: "",
+    feature: "Метис, 6 лет 4 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-zubaka.jpg",
+    name: "Зубака",
+    gender: "male",
+    feature: "Метис, 5 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-geroi.jpg",
+    name: "Герой",
+    gender: "male",
+    feature: "Метис, 7 лет 1 месяц"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-snezhik.jpg",
+    name: "Снежик",
+    gender: "male",
+    feature: "Метис, 5 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-hlopushka.jpg",
+    name: "Хлопушка",
+    gender: "",
+    feature: "Метис, 7 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-rys.jpg",
+    name: "Рысь",
+    gender: "",
+    feature: "Метис, 2 года 5 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-almaz.jpg",
+    name: "Алмаз",
+    gender: "",
+    feature: "Метис, 4 года 12 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-graci.jpg",
+    name: "Грейси",
+    gender: "",
+    feature: "Метис, 4 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-boss.jpg",
+    name: "Босс",
+    gender: "male",
+    feature: "Метис, 7 лет 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-german.jpg",
+    name: "Герман",
+    gender: "male",
+    feature: "Метис, 1 год 1 месяц"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-lovelas.jpg",
+    name: "Ловелас",
+    gender: "male",
+    feature: "Метис, 4 года 2 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-romashka.jpg",
+    name: "Ромашка",
+    gender: "",
+    feature: "Метис, 9 лет 4 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-krepysh.jpg",
+    name: "Крепыш",
+    gender: "male",
+    feature: "Метис, 2 года 4 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-pers.jpg",
+    name: "Перс",
+    gender: "male",
+    feature: "Метис, 5 лет 11 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-abrikos.jpg",
+    name: "Абрикос",
+    gender: "",
+    feature: "Метис, 5 лет 3 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-dobryak.jpg",
+    name: "Добряк",
+    gender: "male",
+    feature: "Метис, 6 лет 3 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-belosnej.jpg",
+    name: "Белоснеж",
+    gender: "male",
+    feature: "Метис, 1 месяц"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-madam.jpg",
+    name: "Мадам",
+    gender: "",
+    feature: "Метис, 8 лет 3 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-manik.jpg",
+    name: "Маник",
+    gender: "male",
+    feature: "Метис, 6 лет 1 месяц"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-mysh.jpg",
+    name: "Мышь",
+    gender: "male",
+    feature: "Метис, 2 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-alia.jpg",
+    name: "Алия",
+    gender: "",
+    feature: "Метис, 3 года 12 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-zhuchok.jpg",
+    name: "Жучок",
+    gender: "male",
+    feature: "Метис, 7 лет 4 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-barabashka.jpg",
+    name: "Барабашка",
+    gender: "",
+    feature: "Метис, 2 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-nectarin.jpg",
+    name: "Нектарин",
+    gender: "male",
+    feature: "Метис, 3 года 8 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-matilda.jpg",
+    name: "Матильда",
+    gender: "",
+    feature: "Метис, 7 лет 3 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-irochka.jpg",
+    name: "Ирочка",
+    gender: "",
+    feature: "Метис, 2 года 9 месяцев"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-poprygunya.jpg",
+    name: "Попрыгуня",
+    gender: "",
+    feature: "Метис, 3 месяца"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-caramelka.jpg",
+    name: "Карамелька",
+    gender: "",
+    feature: "Метис, 3 месяца"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-cleo.jpg",
+    name: "Клео",
+    gender: "",
+    feature: "Метис, 4 года 1 месяц"
+  },
+  {
+    image:
+      "./src/img/dogs/dog-1-1-gercog.jpg",
+    name: "Герцог",
+    gender: "male",
+    feature: "Метис, 5 лет 9 месяцев"
+  },
+  {
+    image:
+      "./src/img/cats/cat-1-1-baron.jpg",
+    name: "Барон",
+    gender: "male",
+    feature: "Метис, 3 года 11 месяцев"
+  },
+];
+const svgMale = '<svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M39 7L26.6571 19.3429M39 7H27.5714M39 7V18.4286M29.8571 27.5714C29.8571 33.8833 24.7404 39 18.4286 39C12.1167 39 7 33.8833 7 27.5714C7 21.2596 12.1167 16.1429 18.4286 16.1429C24.7404 16.1429 29.8571 21.2596 29.8571 27.5714Z" stroke="#FCF8F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+const svgFemale = '<svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M23.5 26.5294C29.8513 26.5294 35 21.2622 35 14.7647C35 8.26724 29.8513 3 23.5 3C17.1487 3 12 8.26724 12 14.7647C12 21.2622 17.1487 26.5294 23.5 26.5294ZM23.5 26.5294V43M16.6 35.9412H30.4" stroke="#FCF8F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+
+
+const pageSize = 12; // Кол-во животных на странице
+let currentPage = 1; // Текущая страница
+
+// Функция отрисовки страницы
+function renderPage() {
+  const container = document.querySelector("#pets");
+  container.innerHTML = ""; // Очищаем прошлую страницу
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const currentPagePets = pets.slice(startIndex, endIndex); // Текущие животные на странице
+
+  // Для каждого элемента массива создаем div и внутрь помещаем информацию о животном
+  currentPagePets.map((pet) => {
+    const petElement = document.createElement("div");
+    petElement.classList.add("pet-card")
+    petElement.innerHTML = `
+      <a href="${pet.link ? pet.link:'javascript:void(0)'}">
+        <img src="${pet.image}"/>
+        <div class="desc">
+          <div class="head-desc">
+            <h3>${pet.name}</h3>
+            <span class="gender">${pet.gender === "male" ? svgMale:svgFemale}</span>
+          </div>
+          <p>${pet.feature ? pet.feature:''}</p>
+        </div>
+      </a>
+      `;
+    container.appendChild(petElement);
+  });
+
+  renderNavigation();
+}
+
+// Функция отрисовки навигации (кнопок)
+function renderNavigation() {
+  const navigationContainer = document.querySelector("#navigation");
+  const prevButton = navigationContainer.querySelector("#prev");
+  const nextButton = navigationContainer.querySelector("#next");
+  const pageCounter = navigationContainer.querySelector("#page-counter");
+
+  // Текущая страница
+  pageCounter.innerHTML = `<span>${currentPage} из ${Math.ceil(pets.length / pageSize)}</span>`
+  
+  // Отключаем/Включаем кнопку назад
+  if (isFirstPage()) {
+    prevButton.setAttribute("disabled", "disabled");
+  } else prevButton.removeAttribute("disabled");
+
+  // Отключаем/Включаем кнопку назад
+  if (isLastPage()) {
+    nextButton.setAttribute("disabled", "disabled");
+  } else nextButton.removeAttribute("disabled");
+}
+
+// Рисуем страницу, когда DOM загружен
+document.addEventListener("DOMContentLoaded", () => {
+  renderPage();
+});
+
+// Проверка последней страницы
+function isLastPage() {
+  return currentPage * pageSize >= pets.length;
+}
+
+// Проверка первой страницы
+function isFirstPage() {
+  return currentPage === 1;
+}
+
+// Функция переключения страниц вперед
+function nextPage() {
+  if(isLastPage()) { // Если последняя страница - пропускаем
+    return;
+  }
+  currentPage++; // +1 к текущей странице
+  renderPage(); // Рисуем новую страницу
+}
+
+// Функция переключения страниц назад
+function prevPage() {
+  if (isFirstPage()) { // Если первая страница - пропускаем
+    currentPage = 1;
+  }
+  currentPage--; // -1 к текущей странице
+  renderPage(); // Рисуем новую страницу
+}
